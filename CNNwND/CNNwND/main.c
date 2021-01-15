ad47ee5ce228d769a3ae3c57593941396e82d7d4
bradenbale <59070001+bradenbale@users.noreply.github.com>
2021-01-15 16:57:00 -0500

    Add files via upload

---
+++ CNNwND/CNNwND/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 1/6/2021 3:39:50 PM
+ *  Author: Braden Bale
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
